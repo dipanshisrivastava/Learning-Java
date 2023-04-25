b293f8bc9674129d9e0f0dab3e4cf909ad940a15
Dipanshi Srivastava <50499214+dipanshisrivastava@users.noreply.github.com>
2023-04-26 00:14:36 +0530

    Update SingleInheritance.java

--- Inheritance/SingleInheritance.java
+++ Inheritance/SingleInheritance.java
@@ -34,7 +34,7 @@ class Student extends Person
     }
 }
 // driver class
-public class SingleInheritence
+public class SingleInheritance
 {
 	public static void main(String[] args) {
 	    Student s1 = new Student();
