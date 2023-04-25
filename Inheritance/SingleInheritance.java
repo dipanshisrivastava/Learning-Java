43f8ac060d5a926f730c5018ae55afa1b3ba4b3e
Dipanshi Srivastava <50499214+dipanshisrivastava@users.noreply.github.com>
2023-04-26 00:12:01 +0530

    Add files via upload
    Single Inheritance, Multilevel Inheritance, Hierarchical Inheritance

---
+++ Inheritance/SingleInheritance.java
@@ -0,0 +1,48 @@
+//Class Person is inherited by class Student
+class Person
+{
+    private int age;
+    private String name;
+    public void setAge(int a)
+    {
+        age = a;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public int getAge()
+    {
+        return (age);
+    }
+    public String getName()
+    {
+        return (name);
+    }
+}
+//class Student is inheriting class Person
+class Student extends Person
+{
+    private int rollno;
+    public void setRollno(int r)
+    {
+        rollno = r;
+    }
+    public int getRollno()
+    {
+        return (rollno);
+    }
+}
+// driver class
+public class SingleInheritence
+{
+	public static void main(String[] args) {
+	    Student s1 = new Student();
+	    s1.setRollno(100);
+	    s1.setName("Riya");
+	    s1.setAge(18);
+	    System.out.println("Rollno: " + s1.getRollno());
+		System.out.println("Name: " + s1.getName());
+		System.out.println("Age: " + s1.getAge());
+	}
+}
