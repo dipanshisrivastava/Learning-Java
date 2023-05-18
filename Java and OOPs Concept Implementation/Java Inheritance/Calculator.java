455c0258089f77b8cb949ce4abe52bc1ba43c145
Dipanshi Srivastava <50499214+dipanshisrivastava@users.noreply.github.com>
2023-05-18 19:19:14 +0530

    Add files via upload

---
+++ Java and OOPs Concept Implementation/Java Inheritance/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        MoreAdvanceCalc obj = new MoreAdvanceCalc();
+
+        System.out.print(obj.add(5,4) + " " + obj.sub(5,4) + " " + obj.mul(5,4) + " " + obj.div(5,4) + " " + obj.power(5,4));
+    }    
+}
+
---
+++ Java and OOPs Concept Implementation/Java Inheritance/MoreAdvanceCalc.java
@@ -0,0 +1,7 @@
+public class MoreAdvanceCalc extends AdvanceCalc
+{
+    public double power(int n1, int n2)
+    {
+        return Math.pow(n1, n2);
+    } 
+}
